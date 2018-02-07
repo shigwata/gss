3cd5d952e42f55bd1c9642854e2bd36ab90404fd
shigwata <shigwata01@gmail.com>
2018-02-07 16:11:14 +0900

    add export csv

--- cmd/export.go
+++ cmd/export.go
@@ -21,9 +21,22 @@
 package cmd
 
 import (
+	"context"
+	"encoding/csv"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"os/user"
+	"path/filepath"
 
 	"github.com/spf13/cobra"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	sheets "google.golang.org/api/sheets/v4"
 )
 
 // exportCmd represents the export command
@@ -36,8 +49,78 @@ and usage of using your command. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
+	Args: cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("export called")
+		var spreadsheetId = args[0]
+
+		// format, err := cmd.Flags().GetString("format")
+		// if err != nil {
+		// 	log.Fatal(err)
+		// }
+
+		ctx := context.Background()
+
+		b, err := ioutil.ReadFile("client_secret.json")
+		if err != nil {
+			log.Fatalf("Unable to read client secret file: %v", err)
+		}
+
+		// If modifying these scopes, delete your previously saved credentials
+		// at ~/.credentials/sheets.googleapis.com-go-quickstart.json
+		config, err := google.ConfigFromJSON(b, "https://www.googleapis.com/auth/spreadsheets.readonly")
+		if err != nil {
+			log.Fatalf("Unable to parse client secret file to config: %v", err)
+		}
+		client := getClient(ctx, config)
+
+		sheetsService, err := sheets.New(client)
+		if err != nil {
+			log.Fatalf("Unable to retrieve Sheets Client %v", err)
+		}
+
+		resp, err := sheetsService.Spreadsheets.Get(spreadsheetId).Do()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if len(resp.Sheets) > 0 {
+			for _, sheet := range resp.Sheets {
+
+				filename := sheet.Properties.Title + ".csv"
+				log.Println(filename)
+				file, err := os.Create(filename)
+				if err != nil {
+					log.Fatal(err)
+				}
+				defer file.Close()
+
+				range_ := sheet.Properties.Title
+				resp, err := sheetsService.Spreadsheets.Values.Get(spreadsheetId, range_).Context(ctx).Do()
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				var records [][]string
+				w := csv.NewWriter(file)
+				for _, row := range resp.Values {
+					var record []string
+					for _, cell := range row {
+						record = append(record, cell.(string))
+					}
+					records = append(records, record)
+				}
+
+				if err := w.WriteAll(records); err != nil {
+					// 書き込みエラー発生
+					fmt.Println("Write error: ", err)
+					return
+				}
+
+			}
+		} else {
+			fmt.Print("No data found.")
+		}
+
 	},
 }
 
@@ -53,4 +136,78 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// exportCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// exportCmd.Flags().StringP("format", "f", "csv", "Export file format")
+
+}
+
+// getClient uses a Context and Config to retrieve a Token
+// then generate a Client. It returns the generated Client.
+func getClient(ctx context.Context, config *oauth2.Config) *http.Client {
+	cacheFile, err := tokenCacheFile()
+	if err != nil {
+		log.Fatalf("Unable to get path to cached credential file. %v", err)
+	}
+	tok, err := tokenFromFile(cacheFile)
+	if err != nil {
+		tok = getTokenFromWeb(config)
+		saveToken(cacheFile, tok)
+	}
+	return config.Client(ctx, tok)
+}
+
+// getTokenFromWeb uses Config to request a Token.
+// It returns the retrieved Token.
+func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
+	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
+	fmt.Printf("Go to the following link in your browser then type the "+
+		"authorization code: \n%v\n", authURL)
+
+	var code string
+	if _, err := fmt.Scan(&code); err != nil {
+		log.Fatalf("Unable to read authorization code %v", err)
+	}
+
+	tok, err := config.Exchange(oauth2.NoContext, code)
+	if err != nil {
+		log.Fatalf("Unable to retrieve token from web %v", err)
+	}
+	return tok
+}
+
+// tokenCacheFile generates credential file path/filename.
+// It returns the generated credential path/filename.
+func tokenCacheFile() (string, error) {
+	usr, err := user.Current()
+	if err != nil {
+		return "", err
+	}
+	tokenCacheDir := filepath.Join(usr.HomeDir, ".credentials")
+	os.MkdirAll(tokenCacheDir, 0700)
+	return filepath.Join(tokenCacheDir,
+		url.QueryEscape("sheets.googleapis.com-go-quickstart.json")), err
+}
+
+// tokenFromFile retrieves a Token from a given file path.
+// It returns the retrieved Token and any read error encountered.
+func tokenFromFile(file string) (*oauth2.Token, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+	t := &oauth2.Token{}
+	err = json.NewDecoder(f).Decode(t)
+	defer f.Close()
+	return t, err
+}
+
+// saveToken uses a file path to create a file and store the
+// token in it.
+func saveToken(file string, token *oauth2.Token) {
+	fmt.Printf("Saving credential file to: %s\n", file)
+	f, err := os.OpenFile(file, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		log.Fatalf("Unable to cache oauth token: %v", err)
+	}
+	defer f.Close()
+	json.NewEncoder(f).Encode(token)
 }
